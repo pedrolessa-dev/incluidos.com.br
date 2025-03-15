030ff7da4fbabea22f323b07705b32424a0e9a3d
Pedro Lessa <122374868+pedrolessa-dev@users.noreply.github.com>
2025-03-15 18:43:57 +0000

    feat: update text page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-  return <h1>Teste deploy</h1>;
+  return (
+    <div>
+      <h1>Página em construção</h1>
+      <p>
+        O Incluídos é uma plataforma aberta para discussão, compartilhamento de
+        conhecimento e experiências sobre deficiência e inclusão. Aqui,
+        profissionais, familiares, pessoas com deficiência e entusiastas podem
+        trocar informações sobre acessibilidade, direitos, tecnologia assistiva,
+        mercado de trabalho e muito mais.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
