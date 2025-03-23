3cb77e1ef17545f60b9ba77fa40e8edc4b563421
Pedro Lessa <122374868+pedrolessa-dev@users.noreply.github.com>
2025-03-23 19:12:38 +0000

    fix: makes `database.js` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,12 +8,20 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+  console.log("Credentials:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   try {
+    await client.connect();
     const res = await client.query(queryObject);
     return res;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
