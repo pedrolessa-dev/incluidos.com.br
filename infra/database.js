b4646698a7532f6b209dc9fa6266804c31ff3ec7
Pedro Lessa <122374868+pedrolessa-dev@users.noreply.github.com>
2025-03-25 22:40:04 +0000

    chore: add logs

--- infra/database.js
+++ infra/database.js
@@ -9,6 +9,15 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     ssl: getSSLValues(),
   });
+  console.log("Credentials:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+    ca: process.env.POSTGRES_CA,
+  });
   try {
     await client.connect();
     const res = await client.query(queryObject);
